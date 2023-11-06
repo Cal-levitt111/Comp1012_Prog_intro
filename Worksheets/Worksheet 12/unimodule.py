42307bff81054f44c793d4864e0be7194d88924c
Cal Levitt <Cal.levitt111@gmail.com>
2023-11-06 17:07:20 +0000

    Not working

--- Worksheets/Worksheet 12/unimodule.py
+++ Worksheets/Worksheet 12/unimodule.py
@@ -38,15 +38,13 @@ class Transcript:
             item.display_details()
 
 
-"""COMP1012 = UniModule("COMP1011", "Intro to Prog.", 1, 10, grade=75, discovery=True)
+COMP1012 = UniModule("COMP1011", "Intro to Prog.", 1, 10, grade=75, discovery=True)
 COMP1121 = UniModule("COMP1121", "Databases", 1, 10, PFP=True)
 COMP1211 = UniModule("COMP1211", "Comp. Arch.", 1, 10, grade=80, PFP=True)
 t_student1 = Transcript()
 t_student1.add_module(COMP1012)
 t_student1.add_module(COMP1121)
 t_student1.add_module(COMP1211)
-t_student1.print_transcript()"""
-
-t_student1 = Transcript()
-COMP1211 = UniModule("COMP1211", "Comp. Arch.", 1, 10, grade=80, PFP=True)
-t_student1.add_module("Test")
+#t_student1.add_module("test1")
+t_student1.add_module(COMP1211)
+t_student1.print_transcript()
