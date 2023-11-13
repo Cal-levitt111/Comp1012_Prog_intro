ff65500f34c137a6339e9c630faf1516469dccc6
Cal Levitt <Cal.levitt111@gmail.com>
2023-11-13 15:14:21 +0000

    XX

--- Worksheets/Worksheet 15/ducky.py
+++ Worksheets/Worksheet 15/ducky.py
@@ -1,14 +1,15 @@
-iterable = ("1 12 72 hello face 4 six")
 def sum_iter_value(iterable):
     iter_to_test = iterable.replace(" ", ",").split(",")
     sum = 0
     for item in iter_to_test:
         if item.isnumeric():
             sum += float(item)
-    return sum
+    return int(sum)
+
 
 def main():
-    print(sum_iter_value(iterable))
+    print(sum_iter_value())
+
 
 if __name__ == "__main__":
     main()
